a9bd1839933156bd4968ea7715b72c03c5d56f76
Zander Lewis <wolfthedev@gmail.com>
2024-06-29 17:00:58 -0400

    Fix Stars

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,5 +41,25 @@
         @stack('modals')
 
         @livewireScripts
+        <script>
+        function createStars(id, count) {
+            for (let i = 0; i < count; i++) {
+                let star = document.createElement('div');
+                star.className = 'star';
+                star.style.top = Math.random() * window.innerHeight + 'px';
+                star.style.left = Math.random() * window.innerWidth + 'px';
+                let size = Math.random() * 3; // Change this value to adjust the range of sizes
+                star.style.width = `${size}px`;
+                star.style.height = `${size}px`;
+                document.getElementById(id).appendChild(star);
+            }
+        }
+
+        window.onload = function() {
+            createStars('stars1', 400);
+            createStars('stars2', 300);
+            createStars('stars3', 200);
+        };
+    </script>
     </body>
 </html>
