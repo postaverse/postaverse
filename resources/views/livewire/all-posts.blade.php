62bc721e457f015735113ccb3013891792ece498
Zander Lewis <wolfthedev@gmail.com>
2024-06-29 22:10:07 -0400

    Update all-posts.blade.php

--- resources/views/livewire/all-posts.blade.php
+++ resources/views/livewire/all-posts.blade.php
@@ -2,16 +2,16 @@
     @foreach ($posts as $post)
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
             <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <h1 class="text-xl font-bold text-gray-900">
+                <h1 class="text-xl font-bold text-white">
                     {{ $post->title }} -
                     {{ $post->created_at->diffForHumans() }} -
                     {{ $post->user->name  }}
                 </h1>
 
-                <p class="text-gray-900">{{ $post->content }}</p>
+                <p class="text-white">{{ $post->content }}</p>
 
                 @if ($post->user_id == auth()->user()->id)
-                    <button class="text-gray-900" wire:click="delete({{ $post->id }})">
+                    <button class="text-white" wire:click="delete({{ $post->id }})">
                         Delete
                     </button>
                 @endif
