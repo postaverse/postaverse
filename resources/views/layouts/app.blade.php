868c0024a5f3eccffe28c4c9a2112756a270a27b
Zander Lewis <wolfthedev@gmail.com>
2024-06-29 21:52:19 -0400

    e again

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,8 @@
 
         <!-- Page Content -->
         <main>
-            
-            <div id="stars1" class="stars"></div>
             {{ $slot }}
+            <div id="stars1" class="stars"></div>
             <div id="stars2" class="stars"></div>
             <div id="stars3" class="stars"></div>
         </main>
