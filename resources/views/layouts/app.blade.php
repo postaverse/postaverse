9523d2bb45d6be2001cea09fbcd0bb69e878f083
Zander Lewis <wolfthedev@gmail.com>
2024-06-29 21:57:43 -0400

    force dark mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,12 +22,12 @@
 <body class="font-sans antialiased">
     <x-banner />
 
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-gray-900">
         @livewire('navigation-menu')
 
         <!-- Page Heading -->
         @if (isset($header))
-        <header class="bg-white dark:bg-gray-800 shadow">
+        <header class="bg-gray-800 shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
