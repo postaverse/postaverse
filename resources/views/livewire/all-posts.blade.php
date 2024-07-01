bd43bc0edb3c2d78bf1853ec85d9258bbef9c970
Zander Lewis <wolfthedev@gmail.com>
2024-06-30 22:42:29 -0400

    Update all-posts.blade.php

--- resources/views/livewire/all-posts.blade.php
+++ resources/views/livewire/all-posts.blade.php
@@ -3,11 +3,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
             <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <h1 class="text-xl font-bold text-white">
-                    {{ $post->title }} -
-                    {{ $post->created_at->diffForHumans() }} -
-                    {{ $post->user->name  }}
+                    {{ $post->title }}
                 </h1>
-
+                <h2 class="text-xl font-bold text-white">
+                    {{ $post->user->name  }}
+                </h2>
+                <h3 class="text-xl font-bold text-white">
+                    {{ $post->created_at->diffForHumans() }} -
+                </h3>
                 <p class="text-white">{{ $post->content }}</p>
 
                 @if ($post->user_id == auth()->user()->id)
