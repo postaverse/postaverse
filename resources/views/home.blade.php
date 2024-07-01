69f18933e7f5baec3adebde0c3439e92d4a0ebc5
Zander Lewis <wolfthedev@gmail.com>
2024-06-30 22:18:27 -0400

    Fix header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <x-slot name="header">
+    <x-slot name="header" class="header">
         <h2 class="font-semibold text-xl text-gray-200 leading-tight">
             {{ __('Home') }}
         </h2>
