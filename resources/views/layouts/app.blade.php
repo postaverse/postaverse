c9cf26ee6f4861a537e6942b34fa6270b28804df
Zander Lewis <wolfthedev@gmail.com>
2024-06-30 22:03:38 -0400

    fix stars

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,36 +37,12 @@
         <!-- Page Content -->
         <main>
             {{ $slot }}
-            <div id="stars1" class="stars"></div>
-            <div id="stars2" class="stars"></div>
-            <div id="stars3" class="stars"></div>
         </main>
     </div>
 
     @stack('modals')
 
     @livewireScripts
-    <script>
-        function createStars(id, count) {
-            for (let i = 0; i < count; i++) {
-                let star = document.createElement('div');
-                star.className = 'star';
-                star.style.top = Math.random() * window.innerHeight + 'px';
-                star.style.left = Math.random() * window.innerWidth + 'px';
-                let size = Math.random() * 3; // Change this value to adjust the range of sizes
-                star.style.width = `${size}px`;
-                star.style.height = `${size}px`;
-                star.style.transform = `rotate(${Math.random() * 360}deg)`;
-                document.getElementById(id).appendChild(star);
-            }
-        }
-
-        window.onload = function() {
-            createStars('stars1', 400);
-            createStars('stars2', 300);
-            createStars('stars3', 200);
-        };
-    </script>
 </body>
 
 </html>
