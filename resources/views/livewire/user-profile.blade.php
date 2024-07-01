f1902f466577e3ea42466eab5a3a700d71f2e117
Zander Lewis <wolfthedev@gmail.com>
2024-07-01 13:16:13 -0400

    bio

--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -4,6 +4,10 @@
         <img src="{{ $user->profile_photo_url }}" alt="Profile Photo" class="w-40 h-40 rounded-full">
         <br>
         <h1 class="text-xxl font-bolder text-white">{{ $user->name }}</h1>
+        <br>
+        @if ($user->bio !== null)
+        <p class="text-white">{{ $user->bio }}</p>
+        @endif
     </div>
     <br>
     @foreach ($posts as $post)
