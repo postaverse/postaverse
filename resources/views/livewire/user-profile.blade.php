ceae707536acd7cf38c319464527920875d0d70c
Zander Lewis <wolfthedev@gmail.com>
2024-07-01 10:20:04 -0400

    Update user-profile.blade.php

--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -1,14 +1,31 @@
 <div>
-    <div class="user-profile">
-        <h1>{{ $user->name }}</h1>
-        <p>{{ $user->bio }}</p>
-        <div class="posts">
-            @foreach ($posts as $post)
-                <div class="post">
-                    <h2>{{ $post->title }}</h2>
-                    <p>{{ $post->content }}</p>
+    @foreach ($posts as $post)
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+            <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <div class="flex items-center space-x-4">
+                    <img src="{{ $post->user->profile_photo_url }}" alt="{{ $post->user->name }}'s profile photo" class="w-10 h-10 rounded-full">
+                    <h2 class="text-lg font-bold text-white">
+                        {{ $post->user->name }}
+                    </h2>
                 </div>
-            @endforeach
+                <h1 class="text-xl font-bold text-white">
+                    {{ $post->title }}
+                </h1>
+                <h3 class="text-base font-bold text-white">
+                    {{ $post->created_at->diffForHumans() }}
+                </h3>
+                <p class="text-white">{{ $post->content }}</p>
+
+                @if ($post->user_id == auth()->user()->id)
+                    <button class="text-red-800 font-bold" wire:click="delete({{ $post->id }})">
+                        Delete
+                    </button>
+                @endif
+            </div>
         </div>
+    @endforeach
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+        {{ $posts->links() }}
     </div>
 </div>
