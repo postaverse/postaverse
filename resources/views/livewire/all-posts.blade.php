748720a4a316ff2a5fd9418f9ba958a89dce3fb2
Zander Lewis <wolfthedev@gmail.com>
2024-07-01 10:23:12 -0400

    fix

--- resources/views/livewire/all-posts.blade.php
+++ resources/views/livewire/all-posts.blade.php
@@ -24,4 +24,8 @@
             </div>
         </div>
     @endforeach
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+        {{ $posts->links() }}
+    </div>
 </div>
--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -24,8 +24,4 @@
             </div>
         </div>
     @endforeach
-
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
-        {{ $posts->links() }}
-    </div>
 </div>
