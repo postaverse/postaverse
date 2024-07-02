bf683a3cf18da4d832c938f31b83370550dc96f9
Zander Lewis <wolfthedev@gmail.com>
2024-07-01 21:28:41 -0400

    Update feed.blade.php

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -1,5 +1,5 @@
-<br>
 <div>
+    <br>
     @foreach ($posts as $post)
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
             <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
