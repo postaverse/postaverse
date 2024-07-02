743e71f57cea049e224112b408bc65bda035871f
Zander Lewis <wolfthedev@gmail.com>
2024-07-02 19:26:42 -0400

    Update search.blade.php

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,17 +1,12 @@
 <div>
-    <br>
-    <x-slot name="header" class="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-200 leading-tight">
             {{ __('Search') }}
         </h2>
     </x-slot>
-    <div class="flex flex-col items-center justify-center main">
-        <br>
-        <input type="text" wire:model="search" class="bg-gray-800 text-white p-2 rounded" placeholder="Search for users...">
-        <br>
-        <br>
+    <div class="flex flex-col items-start justify-start main px-6 lg:px-8">
         @if ($users->isEmpty())
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+        <div class="w-full mb-6">
             <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <h1 class="text-xl font-bold text-white">
                     No users found.
@@ -20,10 +15,9 @@
         </div>
         @else
         @foreach ($users as $user)
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6 main">
+        <div class="w-full mb-6">
             <div wire:key="{{ $user->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <div class="flex items
-                -center space-x-4">
+                <div class="flex items-center space-x-4">
                     <img src="{{ $user->profile_photo_url }}" alt="{{ $user->name }}'s profile photo" class="w-10 h-10 rounded-full">
                     <h2 class="text-lg font-bold text-white">
                         <a href="{{ route('user-profile', $user->id) }}" class="hyperlink">
@@ -36,7 +30,7 @@
         @endforeach
         @endif
         @if ($posts->isEmpty())
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+        <div class="w-full mb-6">
             <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <h1 class="text-xl font-bold text-white">
                     No posts found.
@@ -45,10 +39,9 @@
         </div>
         @else
         @foreach ($posts as $post)
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6 main">
+        <div class="w-full mb-6">
             <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <div class="flex items
-                -center space-x-4">
+                <div class="flex items-center space-x-4">
                     <img src="{{ $post->user->profile_photo_url }}" alt="{{ $post->user->name }}'s profile photo" class="w-10 h-10 rounded-full">
                     <h2 class="text-lg font-bold text-white">
                         <a href="{{ route('user-profile', $post->user->id) }}" class="hyperlink">
