7eafd5f0c04a9848c4493cae41a4c85fea45e425
Zander Lewis <wolfthedev@gmail.com>
2024-07-02 19:21:10 -0400

    Update search.blade.php

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,26 +1,70 @@
-<!-- Search Results -->
 <div>
-    <div class="flex flex-col space-y-4">
-        @if ($searchResults->count() > 0)
-            @foreach ($searchResults as $result)
+    <br>
+    <x-slot name="header" class="header">
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
+            {{ __('Search') }}
+        </h2>
+    </x-slot>
+    <div class="flex flex-col items-center justify-center main">
+        <br>
+        <input type="text" wire:model="search" class="bg-gray-800 text-white p-2 rounded" placeholder="Search for users...">
+        <br>
+        <br>
+        @if ($users->isEmpty())
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+            <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <h1 class="text-xl font-bold text-white">
+                    No users found.
+                </h1>
+            </div>
+        </div>
+        @else
+        @foreach ($users as $user)
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6 main">
+            <div wire:key="{{ $user->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <div class="flex items
                 -center space-x-4">
-                    <img src="{{ $result->profile_photo_url }}" alt="{{ $result->name }}'s profile photo" class="w-10 h-10 rounded-full">
+                    <img src="{{ $user->profile_photo_url }}" alt="{{ $user->name }}'s profile photo" class="w-10 h-10 rounded-full">
                     <h2 class="text-lg font-bold text-white">
-                        <a href="{{ route('user-profile', $result->id) }}" class="hyperlink">
-                            {{ $result->name }}
+                        <a href="{{ route('user-profile', $user->id) }}" class="hyperlink">
+                            {{ $user->name }}
                         </a>
                     </h2>
                 </div>
-            @endforeach
-        @else
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
-                <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
-                    <h1 class="text-xl font-bold text-white">
-                        No users found.
-                    </h1>
-                </div>
             </div>
+        </div>
+        @endforeach
+        @endif
+        @if ($posts->isEmpty())
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+            <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <h1 class="text-xl font-bold text-white">
+                    No posts found.
+                </h1>
+            </div>
+        </div>
+        @else
+        @foreach ($posts as $post)
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6 main">
+            <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <div class="flex items
+                -center space-x-4">
+                    <img src="{{ $post->user->profile_photo_url }}" alt="{{ $post->user->name }}'s profile photo" class="w-10 h-10 rounded-full">
+                    <h2 class="text-lg font-bold text-white">
+                        <a href="{{ route('user-profile', $post->user->id) }}" class="hyperlink">
+                            {{ $post->user->name }}
+                        </a>
+                    </h2>
+                </div>
+                <h1 class="text-xl font-bold text-white">
+                    {{ $post->title }}
+                </h1>
+                <h3 class="text-base font-bold text-white">
+                    {{ $post->created_at->diffForHumans() }}
+                </h3>
+            </div>
+        </div>
+        @endforeach
         @endif
     </div>
 </div>
