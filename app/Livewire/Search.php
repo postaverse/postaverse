d464beda2e8475447aa5d45a9208a1a464ec7fd5
Zander Lewis <wolfthedev@gmail.com>
2024-07-02 18:55:44 -0400

    Update Search.php

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -12,15 +12,15 @@ class Search extends Component
     public function index(Request $request)
     {
         $query = $request->input('query');
-
         // Search users
         $users = User::where('name', 'LIKE', "%{$query}%")->get();
-
         // Search posts
         $posts = Post::where('title', 'LIKE', "%{$query}%")
                      ->orWhere('content', 'LIKE', "%{$query}%")
                      ->get();
-
-        return view('search.results', compact('users', 'posts'));
+        // Combine users and posts into one collection if needed
+        $searchResults = $users->concat($posts);
+        // Alternatively, you can pass users and posts separately if they need to be handled differently in the view
+        return view('search.results', compact('searchResults'));
     }
 }
