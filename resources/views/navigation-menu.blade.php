1764abb04b65e4d66631425e3421e045683c16ff
Zander Lewis <wolfthedev@gmail.com>
2024-07-01 20:52:17 -0400

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Home') }}
                     </x-nav-link>
-                    <x-nav-link href="route('feed')" :active="request()->routeIs('feed')">
+                    <x-nav-link href="{{ route('feed') }}" :active="request()->routeIs('feed')">
                         {{ __('Feed') }}
                     </x-nav-link>
                 </div>
