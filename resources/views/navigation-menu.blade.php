d988d07553db6cd8d3d7bd8aa46e36e526f9e8ac
Zander Lewis <wolfthedev@gmail.com>
2024-07-01 22:27:30 -0400

    Revert "Update navigation-menu.blade.php"
    This reverts commit bbbdcc533e33aa47cb6c7240d3b6e152c20defd5.

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,6 +1,6 @@
-<nav x-data="{ open: false }" class="bg-gray-800 border-b border-gray-700">
+<nav x-data="{ open: false }" class="bg-gray-800 border-b border-gray-700 main">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 main">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
