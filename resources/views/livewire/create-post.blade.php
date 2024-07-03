c8634d92b32cc0bab2fcda77424ccf128b2b839c
Zander Lewis <wolfthedev@gmail.com>
2024-07-03 11:35:02 -0400

    Update create-post.blade.php

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,7 +1,6 @@
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
     <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
-
-        <form wire:submit="submit">
+        <form wire:submit.prevent="submit">
             <div class="mb-4">
                 <x-label for="title" value="{{ __('Title') }}" />
                 <x-input id="title" type="text" class="mt-1 block w-full" wire:model="title" />
@@ -10,7 +9,6 @@
                 </div>
                 @error('title') <span class="error text-red-500">{{ $message }}</span> @enderror
             </div>
-
             <div class="mb-4">
                 <x-label for="content" value="{{ __('Post') }}" />
                 <x-textarea id="content" class="mt-1 block w-full" wire:model="content" />
@@ -19,10 +17,7 @@
                 </div>
                 @error('content') <span class="error text-red-500">{{ $message }}</span> @enderror
             </div>
-
-            <x-button type="submit">
-                {{ __('Post') }}
-            </x-button>
+            <x-button type="submit">{{ __('Submit') }}</x-button>
         </form>
     </div>
 </div>
