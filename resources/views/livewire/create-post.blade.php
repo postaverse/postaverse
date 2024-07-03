90474a22e248517d989922af6f7a926787c84bc0
Zander Lewis <wolfthedev@gmail.com>
2024-07-03 11:47:47 -0400

    fix

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,19 +1,19 @@
-<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6" x-data="{ title: '', content: '' }">
     <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
         <form wire:submit.prevent="submit">
             <div class="mb-4">
                 <x-label for="title" value="{{ __('Title') }}" />
-                <x-input id="title" type="text" class="mt-1 block w-full" wire:model="title" />
+                <x-input id="title" type="text" class="mt-1 block w-full" wire:model="title" x-model="title" />
                 <div class="mt-1 text-sm text-gray-600">
-                    Characters: {{ strlen($title) }} / 100
+                    Characters: <span x-text="title.length"></span> / 100
                 </div>
                 @error('title') <span class="error text-red-500">{{ $message }}</span> @enderror
             </div>
             <div class="mb-4">
                 <x-label for="content" value="{{ __('Post') }}" />
-                <x-textarea id="content" class="mt-1 block w-full" wire:model="content" />
+                <x-textarea id="content" class="mt-1 block w-full" wire:model="content" x-model="content" />
                 <div class="mt-1 text-sm text-gray-600">
-                    Characters: {{ strlen($content) }} / 500
+                    Characters: <span x-text="content.length"></span> / 500
                 </div>
                 @error('content') <span class="error text-red-500">{{ $message }}</span> @enderror
             </div>
