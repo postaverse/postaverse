d0c9218bfac2bec79a887ff65451569e4e944e12
Zander Lewis <wolfthedev@gmail.com>
2024-07-09 09:27:05 -0400

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
