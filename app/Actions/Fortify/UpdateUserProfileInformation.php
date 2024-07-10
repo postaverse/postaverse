2fc6fedb411cd70e7572caa3419870bcf9c70c05
Zander Lewis <wolfthedev@gmail.com>
2024-07-09 21:15:53 -0400

    Update UpdateUserProfileInformation.php

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -20,7 +20,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
+            'photo' => ['nullable', 'mimes:jpg,jpeg,png,webp,heic', 'max:1024'],
             'bio' => ['nullable', 'max:1024'],
             'handle' => ['nullable', 'string', 'max:16', Rule::unique('users')->ignore($user->id)],
         ])->validateWithBag('updateProfileInformation');
