82d039d48b338d47f0d72f423a0c32de68113f96
Zander Lewis <wolfthedev@gmail.com>
2024-07-17 20:22:53 -0400

    Update redeem-meteors.blade.php

--- resources/views/livewire/redeem-meteors.blade.php
+++ resources/views/livewire/redeem-meteors.blade.php
@@ -1,19 +1,18 @@
 <div class="max-w-7xl mx-auto">
-    <div class="text-center pb-1">
-        @if (session()->has('message'))
-        <div class="alert alert-success text-green-500">
-            {{ session('message') }}
-        </div>
-        @elseif (session()->has('error'))
-        <div class="alert alert-danger text-red-500">
-            {{ session('error') }}
-        </div>
-        @endif
-    </div>
     <div>
         <button wire:click="redeemMeteors" class="bg-gray-400 hover:bg-gray-600 text-black font-bold py-2 px-4 rounded">
             <img src="{{ asset('images/meteor.png') }}" class="w-6 h-6 inline-block" alt="Meteor">
+            @if (session()->has('error'))
+            <span class="text-red-500">
+            {{ session('error') }}
+            </span>
+            @elseif (session()->has('message'))
+            <span class="text-green-500">
+            {{ session('message') }}
+            </span>
+            @else
             Redeem your daily Meteors!
+            @endif
         </button>
     </div>
 </div>
