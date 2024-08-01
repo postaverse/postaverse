0b2970cd8485b8a0f2f41505cc5cd5764880f9c6
Zander Lewis <wolfthedev@gmail.com>
2024-07-31 21:19:17 -0400

    Update shop-text-themes.blade.php

--- resources/views/livewire/shop-text-themes.blade.php
+++ resources/views/livewire/shop-text-themes.blade.php
@@ -6,7 +6,7 @@
             </h2>
         </div>
     </x-slot>
-    <h1 class="text-4xl font-bold mb-6 text-center">Shop Text Themes</h1>
+    <h1 class="text-4xl font-bold mb-6 text-center">Username Themes</h1>
     <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         @foreach($themes as $theme)
 
