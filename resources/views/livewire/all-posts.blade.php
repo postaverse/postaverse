cfd96416342bc1fbca14ad7a353c8abb057f2eee
Zander Lewis <wolfthedev@gmail.com>
2024-08-03 18:38:25 -0400

    Update all-posts.blade.php

--- resources/views/livewire/all-posts.blade.php
+++ resources/views/livewire/all-posts.blade.php
@@ -83,7 +83,7 @@
                         <div class="flex -space-x-4">
                             @foreach ($post->likes->take(5) as $like)
                                 <img src="{{ $like->user->profile_photo_url }}"
-                                    alt="{{ $like->user->name }}'s profile photo" class="w-6 h-6 rounded-full">
+                                    alt="{{ $like->user->name }}'s profile photo" class="w-10 h-10 rounded-full border-2 border-gray-800">
                             @endforeach
                             @if ($post->likes->count() > 5)
                                 <span class="text-white">+{{ $post->likes->count() - 5 }}</span>
