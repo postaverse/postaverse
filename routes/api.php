98ef11291c4e67a012f4bdfdde39dd37ff178a4e
Zander Lewis <wolfthedev@gmail.com>
2024-08-03 21:11:04 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/post/{id}', Post::class)->name('post');
