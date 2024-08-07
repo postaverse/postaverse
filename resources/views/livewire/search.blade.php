404ec0228d643ce254e1dc80b8ac54e3ed7d61ac
Zander Lewis <wolfthedev@gmail.com>
2024-08-07 12:50:44 -0400

    Update search.blade.php

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -55,7 +55,7 @@
             </h1>
             @foreach ($posts as $post)
                 <div class="w-full mb-6">
-                    <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4 flex items-center justify-between">
+                    <div wire:key="{{ $post->id }}" class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
                         <div class="flex items-center space-x-4">
                             <img src="{{ $post->user->profile_photo_url }}"
                                 alt="{{ $post->user->name }}'s profile photo" class="w-10 h-10 rounded-full">
