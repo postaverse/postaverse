9de88614b5c8d56db593d072e1da621db24e5e2b
Zander Lewis <zander@zanderlewis.dev>
2024-08-09 16:11:04 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,8 @@
             <!--
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7453651531634667"
                 crossorigin="anonymous"></script>
-            <!-- Horizontal 1 -->
+             Horizontal 1 -->
+            <!--
             <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7453651531634667"
                 data-ad-slot="6397338332" data-ad-format="auto" data-full-width-responsive="true"></ins>
             <script>
