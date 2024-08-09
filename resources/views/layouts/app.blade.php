afd9aa03044bdf13064afd2bed7e3844c544ae1d
Zander Lewis <zander@zanderlewis.dev>
2024-08-09 11:02:58 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,11 +50,11 @@
 
     <footer class="bg-gray-800">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <div class="flex flex-col items-center space-y-4">
+            <div class="flex flex-col items-center space-x-8">
                 <div class="text-gray-400 text-sm">
                     &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
                 </div>
-                <div class="flex justify-center space-x-8">
+                <div class="flex justify-center">
                     <div class="text-gray-400 text-sm">
                         <h2 class="text-lg font-bold text-gray-300">Contributors</h2>
                     </div>
