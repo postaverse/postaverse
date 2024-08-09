e0aa07ddb1348ae24768e5ee3bd96285238df013
Zander Lewis <zander@zanderlewis.dev>
2024-08-08 20:33:20 -0400

    fix

--- config/app.php
+++ config/app.php
@@ -122,21 +122,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
-    'providers' => [
-        App\Providers\AppServiceProvider::class,
-        App\Providers\SocialstreamServiceProvider::class,
-        \SocialiteProviders\Manager\ServiceProvider::class,
-    ],
 ];
