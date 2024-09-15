8f24fe345c122545b8dad4dcefc9b7544686a878
Zander Lewis <zander@zanderlewis.dev>
2024-09-14 22:01:55 -0400

    admin dash

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
-use Laravel\Pulse\Facades\Pulse;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,13 +24,5 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('viewPulse', function (User $user) {
             return $user->admin_rank >= 1;
         });
-
-        Pulse::user(fn ($user) => [
-            'id' => $user->id,
-            'name' => $user->name,
-            'email' => $user->email,
-            'admin_rank' => $user->admin_rank,
-            'avatar' => $user->avatar_url,
-        ]);
     }
 }
