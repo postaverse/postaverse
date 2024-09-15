1cba95b9e0281eaedc494a9535044b48ed56c05a
Zander Lewis <zander@zanderlewis.dev>
2024-09-14 21:37:53 -0400

    fix

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Gate::define('viewPulse', function (User $user) {
+            return $user->admin_rank >= 1;
+        });
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     App\Providers\SocialstreamServiceProvider::class,
-    \SocialiteProviders\Manager\ServiceProvider::class,
+    SocialiteProviders\Manager\ServiceProvider::class,
 ];
