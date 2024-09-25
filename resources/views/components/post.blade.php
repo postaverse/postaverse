fac14adbf6cdabf9f5570b816bf314ba33c81cae
Zander Lewis <zander@zanderlewis.dev>
2024-09-25 19:40:19 -0400

    Fix

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,10 +1,10 @@
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6" wire:key="{{ $post->id }}">
-    {{-- Check if post owner is blocked --}}
-    {{-- Split each user_id by the comma --}}
-    @php
-        $blockedUsers = auth()->user()->blockedUsers->pluck('blocked_users')->toArray();
-        $blockedUsers = array_map('trim', explode(',', implode(',', $blockedUsers)));
-    @endphp
+    @if (auth()->user())
+        @php
+            $blockedUsers = auth()->user()->blockedUsers->pluck('blocked_users')->toArray();
+            $blockedUsers = array_map('trim', explode(',', implode(',', $blockedUsers)));
+        @endphp
+    @endif
     @if (!in_array($post->user_id, $blockedUsers))
         <div class="bg-gray-800 border border-gray-700 rounded-lg shadow-md p-4 flex">
             <!-- Post Content -->
