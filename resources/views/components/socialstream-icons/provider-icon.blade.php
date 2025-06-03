d64481faed88d9a564aa307b31a47bfb22f504ad
Zander Lewis <zander@zanderlewis.dev>
2025-06-03 17:43:28 -0400

    remove reddit p2

--- resources/views/components/socialstream-icons/provider-icon.blade.php
+++ resources/views/components/socialstream-icons/provider-icon.blade.php
@@ -41,8 +41,10 @@
         @default
             @if ($provider == 'discord')
                 <x-socialstream-icons.discord {{ $attributes }} />
+            {{--
             @elseif ($provider == 'reddit')
                 <x-socialstream-icons.reddit {{ $attributes }} />
+            --}}
             @else
                 <p>Unknown provider</p>
             @endif
